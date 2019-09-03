80db429e8b7c009ff1184d8c71aa66bcb0d22043
鲷鱼 <2382391761@qq.com>
2019-09-03 17:52:37 +0800

    Create main.cpp

---
+++ 我来了9.3/我来了9.3/main.cpp
@@ -0,0 +1,69 @@
+//
+//  main.cpp
+//  我来了9.3
+//
+//  Created by s20181102933 on 2019/9/3.
+//  Copyright © 2019 s20181102933. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>//malloc函数
+
+struct Node {    //链表结点
+    int data;    //数据
+    struct Node* link;    //指向下一个结点的指针
+};
+
+/* 尾插法建立单链表：返回单链表的头指针 */
+struct Node* buildLinkedList(int* arr, int n); /* 尾插法建立单链表 */
+void printLinkedList(struct Node* head);       /* 打印链表 */
+
+int main(int argc, char const *argv[]) {
+    int n, i;
+    int* a;
+    scanf("%d", &n);
+    a = (int*)malloc(n * sizeof(int));    //动态内存分配申请数组空间
+    for (i = 0; i < n; ++i) {
+        scanf("%d", &a[i]);
+    }
+    
+    struct Node* head = NULL;    //声明一个指针变量head
+    
+    //创建链表，把返回的头指针赋值给head指针变量
+    head = buildLinkedList(a, n);
+    
+    //打印链表：整个链表用head来代表。
+    printLinkedList(head);
+    
+    free(a);    //释放存储空间
+    
+    return 0;
+}
+
+struct Node* buildLinkedList(int* arr, int n)
+{
+    struct Node *head=(struct Node*)malloc(sizeof(struct Node)),*last=NULL;
+    struct Node *h=head;
+    int i=0;
+    while(i<n){
+        last=head;
+        head->data=arr[i];
+        head=(struct Node*)malloc(sizeof(struct Node));
+        last->link=head;
+        i++;
+    }
+    if (n!=0) {
+        last->link=NULL;
+    }
+    return h;
+}
+void printLinkedList(struct Node* head)
+{
+    struct Node* p=head;
+    while(p){
+        printf("%d",p->data);
+        p=p->link;
+        if(p)
+            printf(" ");
+    }
+}
